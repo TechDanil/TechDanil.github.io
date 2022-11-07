76fa121e7fa804b6684729fe683a5a3ed0273f0c
TechDanil <69108171+TechDanil@users.noreply.github.com>
2022-11-07 15:07:06 +0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector('.menu');
+const menuItems = document.querySelectorAll('.menu_item');
+const hamburger = document.querySelector('.hamburger');
+
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');
+});
+
+menuItems.forEach(item => item.addEventListener('click', () => {
+    hamburger.classList.toggle('hamburger_active');
+    menu.classList.toggle('menu_active');    
+}));
+
+
